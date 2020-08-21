020ed92d6bd3f69e40826c70c99e144481c82c86
Jared <jaredturck9@gmail.com>
2020-08-21 14:09:16 +0100

    ..

--- Python/!PYTHON/py/screen recorder/backup/backup 2/main.py
+++ Python/!PYTHON/py/screen recorder/backup/backup 2/main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import time, os, cv2, numpy, gc, threading, keyboard
 from PIL import ImageGrab, Image
 from io import StringIO
@@ -14,17 +13,10 @@ from io import StringIO
 # - start, instead of using for loop, if user presses exit button, close program        done
 # - compress images stored in RAM
 
-=======
-import win32gui, win32ui, win32con, time, os, cv2, glob
-
-# 28 in 1 sec - with date written to HDD
-# 30 in 1 sec - without saving bitmap
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
 
 class screen_recorder():
     def __init__(self):
         self.res_w = 1920
-<<<<<<< HEAD
         self.res_h = 1080
         self.count = 0
         self.root_dir = os.path.dirname(os.path.realpath(__file__))
@@ -70,28 +62,12 @@ class screen_recorder():
             self.error_handler("[-] Memory Overflow error, some frames will be missed!")
         
     def benchmark(self, timeout=1, display=True):
-=======
-        self.red_h = 1080
-        self.count = 0
-        self.root_dir = os.path.dirname(os.path.realpath(__file__))
-        self.img_folder = "img"
-    
-    def take_screenshot(self):
-        pass
-
-    def take_speedshot_2(self):
-        pass
-
-
-    def benchmark(self, timeout=1):
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
         count = 0
         end_loop = time.time() + (timeout)
         while time.time() < end_loop:
             self.take_screenshot()
             count += 1
 
-<<<<<<< HEAD
         if display == True:
             print("Result:",count,"frames in",timeout,"seconds!")
             print("Average:",count/timeout,"frames per second!")
@@ -151,37 +127,3 @@ class screen_recorder():
 
 r = screen_recorder()
 r.start(sec=10)
-=======
-        print("Result:",count,"pixels in",timeout,"seconds!")
-        print("Average:",count/timeout,"pixels per second!")
-        return [timeout, count, count/timeout]
-
-    def func_speed(self):
-        start = time.time()
-        self.take_screenshot()
-        return time.time() - start
-
-    def start(self):
-        while True:
-            self.take_screenshot()
-
-    def convert2video(self):
-        vid_name = "video1.avi"
-
-        images = sorted([i for i in os.listdir(self.img_folder) if i.endswith(".bmp")])
-        frame = cv2.imread(os.path.join(self.img_folder, images[0]))
-        
-        video = cv2.VideoWriter(vid_name, 0, 1, (frame.shape[1], frame.shape[0]))
-        for image in images:
-            video.write(cv2.imread(os.path.join(self.img_folder, image)))
-
-        cv2.destroyAllWindows()
-        video.release()
-
-        #delete all img's after finished recording
-        for f in glob.glob(os.path.join(self.root_dir, self.img_folder)):
-            os.remove(f)
-
-r = screen_recorder()
-#r.start()
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
--- Python/!PYTHON/py/screen recorder/main.py
+++ Python/!PYTHON/py/screen recorder/main.py
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
 import time, os, cv2, numpy, gc, threading, keyboard
 from PIL import ImageGrab, Image
 from io import BytesIO
-=======
-import time, os, cv2, numpy, gc, threading
-from PIL import ImageGrab, Image
-from io import StringIO
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
 
 # 28 in 1 sec - with date written to HDD
 # 30 in 1 sec - without saving bitmap
@@ -15,51 +9,32 @@ from io import StringIO
 # to do
 # - add multi threading so another thread managed writing of video to disk              done
 # - mix colours                                                                         done
-<<<<<<< HEAD
 # - add shortcut key to stop recording                                                  done
 # - start, instead of using for loop, if user presses exit button, close program        done
 # - compress images stored in RAM
 # - there can only be one writer thread, else frames are written in the wrong order
-=======
-# - add shortcut key to stop recording
-# - start, instead of using for loop, if user presses exit button, close program
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
 
 
 class screen_recorder():
     def __init__(self):
         self.res_w = 1920
-<<<<<<< HEAD
         self.res_h = 1080
-=======
-        self.red_h = 1080
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
         self.count = 0
         self.root_dir = os.path.dirname(os.path.realpath(__file__))
         self.frames = {}
         self.frame_counter = 0
-<<<<<<< HEAD
         self.total_frames = 0
-=======
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
         self.current_frame = 0
         self.between_frames_timeout = 0.01
         self.filecounter = len([i for i in os.listdir(".") if os.path.isfile(i) and i.endswith(".avi")])
         self.avg_fps = int(self.benchmark(timeout=1, display=False)[1])
         self.vid_name = "vid_" + str(self.filecounter) + ".avi"
-<<<<<<< HEAD
         self.video = cv2.VideoWriter(self.vid_name, 0, self.avg_fps-1, (self.res_w, self.res_h))
         self.exit = False
         self.error_timeout = 0.1
         self.error_count = 0
         self.running_time = 0
         self.optimised_frames = 0
-=======
-        self.video = cv2.VideoWriter(self.vid_name, 0, self.avg_fps-1, (self.res_w, self.red_h))
-        self.exit = False
-        self.error_timeout = 0.1
-        self.error_count = 0
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
 
     def reset(self):
         self.frames = {}
@@ -83,10 +58,7 @@ class screen_recorder():
         try:
             self.frames[self.frame_counter] = ImageGrab.grab()
             self.frame_counter += 1
-<<<<<<< HEAD
             self.total_frames += 1
-=======
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
             time.sleep(self.between_frames_timeout)
         except MemoryError:
             self.error_handler("[-] Memory Overflow error, some frames will be missed!")
@@ -118,11 +90,7 @@ class screen_recorder():
                 self.current_frame += 1
                 
         gc.collect()
-<<<<<<< HEAD
         
-=======
-
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
     def write2video_loop(self):
         while self.exit == False:
             try:
@@ -135,7 +103,6 @@ class screen_recorder():
         self.video.release()
         self.reset()
 
-<<<<<<< HEAD
     def wait4exit(self, sec):
         while self.running_time < sec:
             if keyboard.is_pressed("x") == True:
@@ -172,15 +139,3 @@ class screen_recorder():
 
 r = screen_recorder()
 r.start()
-=======
-    def start(self, sec=1):
-        threading.Thread(target = self.write2video_loop).start()
-        for i in range(sec):
-            for i in range(self.avg_fps):
-                self.take_screenshot()
-        time.sleep(2)
-        self.exit = True
-
-r = screen_recorder()
-#r.start(sec=600)
->>>>>>> f512dd3c9d5dc6925668413d8444a1645a6f7535
