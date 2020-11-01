cd466eeb3ae5f34a85da01040a7315a6d481a6a5
Jared <jaredturck9@gmail.com>
2020-11-01 21:36:17 +0000

    removed bot

--- Python/!PYTHON/py/discord bot/Discord bot/start.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-C:
-cd "C:\OneDrive\Backup\Jared's Desktop 2\Desktop\Discord bot"
-
-start /min node .
-start /min python main.py
--- Python/!PYTHON/py/discord bot/scripts/get_steam_stats.py
+++
@@ -1,4 +0,0 @@
-from urllib.request import urlopen
-
-url = "https://steamcommunity.com/id/jaredcat"
-html = urlopen(url + "?xml=1").read()
